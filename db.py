35d0e500479f04227c804acfc0771cfe253263ee
Juraj Golian <118574181+Dukoooo@users.noreply.github.com>
2025-12-05 09:38:53 +0100

    Feat: pridat_ulohu def done

--- db.py
+++ db.py
@@ -1,44 +1,79 @@
 import mysql.connector  #prepojenie mysql
 from dotenv import load_dotenv  # modul python-dotenv (na načítanie .env súboru)
 import os  # štandardná knižnica Pythonu (práca s OS a premennými prostredia)
-
+from datetime import datetime
 
 load_dotenv() # spustenie modulu
 
 
-#pripojenie databázy / servera
-try:
-    conn = mysql.connector.connect(
-         host=os.getenv("DB_HOST"),
-         user=os.getenv("DB_USER"),
-         password=os.getenv("DB_PASSWORD"),
-         database=os.getenv("DB_NAME")
-    )
-    print("Pripojenie k databáze bolo úspešné.")
-
-except mysql.connector.Error as err:
-    print(f"Chyba spojenia: {err}")
-    exit()  # Pokiaľ sa nepripojí, vypíše sa chybová hláška a program sa zavrie
-
-# Vytvorenie kurzoru 
-cursor = conn.cursor()
-
-try:
-    cursor.execute('''
-        CREATE TABLE IF NOT EXISTS testovacia_tabulka (
-            id INT AUTO_INCREMENT PRIMARY KEY,
-            meno VARCHAR(100),
-            vek INT
+# 01. pripojenie databázy / servera
+def pripojenie_db():
+    try:
+        conn = mysql.connector.connect(
+            host=os.getenv("DB_HOST"),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD"),
+            database=os.getenv("DB_NAME")
         )
-    ''')
-    print("Tabuľka 'testovacia_tabulka' bola vytvorená.")
-except mysql.connector.Error as err:
-    print(f"Chyba pri vytváraní tabuľky: {err}")
+        return conn
+    except mysql.connector.Error as err:
+        print(f"Chyba spojenia: {err}")
+        exit()  # Pokiaľ sa nepripojí, vypíše sa chybová hláška a program sa zavrie
 
-# Uloženie zmien (Dôležité)
-conn.commit()
+
+
+# 02. Vytvorenie tabulky ak neexistuje
+def vytvorenie_tabulky(conn):
+    try:
+        cursor = conn.cursor() #vytvorenie kurzora na komunikáciu
+        cursor.execute('''
+            CREATE TABLE IF NOT EXISTS ulohy (
+                id INT AUTO_INCREMENT PRIMARY KEY,
+                nazov VARCHAR(255) NOT NULL,
+                popis TEXT,
+                stav ENUM('nezahájeno', 'hotovo', 'probíhá') NOT NULL DEFAULT 'nezahájeno',
+                datum_vytvorenia DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+            )
+        ''')
+
+    # Uloženie zmien (Dôležité)
+        conn.commit()
+        print("Tabuľka 'testovacia_tabulka' bola vytvorená.")
+    except mysql.connector.Error as err:
+        print(f"Chyba pri vytváraní tabuľky: {err}")
+
+
+
+
+
+
+# 03. Pridanie úlohy
+def pridat_ulohu(conn):
+    cursor = conn.cursor()
+    while True:
+        nazov = input("Zadajte názov úlohy:\n ").strip()
+        if not nazov:
+            print("Názov nesmie byť prázdny.")
+            continue
+
+        popis = input("Zadejte popis úlohy:\n ").strip()
+        if not popis:
+            print("Popis nesmie byť prázdny.")
+            continue
+
+        try:
+            cursor.execute(
+                "INSERT INTO ulohy (nazov, popis) VALUES (%s, %s)",
+                (nazov, popis)
+            )
+            conn.commit()
+            print("Úlohy boli úspešne pridané.")
+            break
+        except mysql.connector.Error as err:
+            print(f"Chyba pri pridávaní úlohy: {err}")
+            break
 
 # uzavrenie kurzoru po pripojení
-cursor.close()
-conn.close()
-print("Pripojenie k databáze bolo uzavrené.")
+    cursor.close()
+    conn.close()
+    print("Pripojenie k databáze bolo uzavrené.")
